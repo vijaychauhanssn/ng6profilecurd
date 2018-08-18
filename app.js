b9fdfb3d6bac24b51696a79698e8e667fe7e2346
vijay chauhan <vijaychauhanssn@gmail.com>
2018-08-18 09:19:08 +0530

    filejs

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-
-
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var multer = require("multer");
 var app = express();
-var path = require('path');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,18 +12,8 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.post("/upload",  function(req, res) {
+app.post("/upload", multer({dest: "./uploads/"}).array("uploads[]", 12), function(req, res) {
     res.send(req.files);
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files, path) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'uploads/uploads  ' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
 });
 
 var server = app.listen(3000, function() {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
+import { HttpClient } from '@angular/common/http';
+
 import { NavigationCancel,
   Event,
   NavigationEnd,
@@ -14,10 +16,11 @@ import { NavigationCancel,
 })
 export class AppComponent {
   title = 'Profile';
-  constructor(private _loadingBar: SlimLoadingBarService, private _router: Router) {
+  constructor(private _loadingBar: SlimLoadingBarService, private _router: Router, private http:HttpClient) {
     this._router.events.subscribe((event: Event) => {
       this.navigationInterceptor(event);
     });
+            this.filesToUpload = [];
   }
   private navigationInterceptor(event: Event): void {
     if (event instanceof NavigationStart) {
@@ -33,4 +36,53 @@ export class AppComponent {
       this._loadingBar.stop();
     }
   }
+result:any;
+  imgupload(filetoupload){
+    const obj = {
+      filetoupload:filetoupload
+    }
+     this.http.post('http://localhost:3000/upload', obj).subscribe((result) => {
+            console.log(result);
+        }, (error) => {
+            console.error(error);
+        });
+    }
+  
+
+  filesToUpload: Array<File>;
+
+
+    upload() {
+        this.makeFileRequest("http://localhost:3000/upload", [], this.filesToUpload).then((result) => {
+            console.log(result);
+        }, (error) => {
+            console.error(error);
+        });
+    }
+
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>> fileInput.target.files;
+    }
+
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+        return new Promise((resolve, reject) => {
+            var formData: any = new FormData();
+            var xhr = new XMLHttpRequest();
+            for(var i = 0; i < files.length; i++) {
+                formData.append("uploads[]", files[i], files[i].name);
+            }
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.response));
+                    } else {
+                        reject(xhr.response);
+                    }
+                }
+            }
+            xhr.open("POST", url, true);
+            xhr.send(formData);
+        });
+    }
+
 }
